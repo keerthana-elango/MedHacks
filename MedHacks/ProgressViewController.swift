056314dc74dc786f6c7108fe432aa6d89326ece2
Olivia Brown <obrown917@gmail.com>
2016-09-24 14:45:58 -0400

    Attempted to Add Charts Manually

--- MedHacks/ProgressViewController.swift
+++ MedHacks/ProgressViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import Charts
 
 class ProgressViewController: UIViewController {
 
@@ -15,44 +14,4 @@ class ProgressViewController: UIViewController {
     @IBOutlet weak var pieChartView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
-        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0]
-        
-        setChart(months, values: unitsSold)
-        
-    }
-    
-    func setChart(dataPoints: [String], values: [Double]) {
-        
-        var dataEntries: [ChartDataEntry] = []
-        
-        for i in 0..<dataPoints.count {
-            let dataEntry = ChartDataEntry(value: values[i], xIndex: i)
-            dataEntries.append(dataEntry)
-        }
-        
-        let pieChartDataSet = PieChartDataSet(yVals: dataEntries, label: "Units Sold")
-        let pieChartData = PieChartData(xVals: dataPoints, dataSet: pieChartDataSet)
-        pieChartView.data = pieChartData
-        
-        var colors: [UIColor] = []
-        
-        for i in 0..<dataPoints.count {
-            let red = Double(arc4random_uniform(256))
-            let green = Double(arc4random_uniform(256))
-            let blue = Double(arc4random_uniform(256))
-            
-            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
-            colors.append(color)
-        }
-        
-        pieChartDataSet.colors = colors
-        
-        
-        let lineChartDataSet = LineChartDataSet(yVals: dataEntries, label: "Units Sold")
-        let lineChartData = LineChartData(xVals: dataPoints, dataSet: lineChartDataSet)
-        lineChartView.data = lineChartData
-        
-    }
-    
-}
+	}}

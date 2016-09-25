80c4eba77bee923954cf79e0da96b3cf658f4b0c
Olivia Brown <obrown917@gmail.com>
2016-09-25 09:50:53 -0400

    Revert "Unknown Crash Error"
    This reverts commit efda56ad77647b65885d03fbd8bf5e389c934592.

--- MedHacks/ProgressViewController.swift
+++ MedHacks/ProgressViewController.swift
@@ -25,60 +25,11 @@ class ProgressViewController: UIViewController {
 		}
 
 	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
 		
-//		@IBOutlet weak var lineChartView: LineChartView!
-//		@IBOutlet weak var pieChartView: PieChartView!
-//		
-//		override func viewDidLoad() {
-//			super.viewDidLoad()
-//			
-//			let emotions = ["Anger", "Disgust", "Fear", "Joy", "Sadness"]
-//			let percentage = [36.0, 19.0, 6.0, 13.0, 26.0]
-//			
-//			setChart(dataPoints: emotions, values: percentage)
-//		}
-//		
-//		func setChart(dataPoints: [String], values: [Double]) {
-//			
-//			var dataEntries: [ChartDataEntry] = []
-//			
-//			for i in 0..<dataPoints.count {
-//				let dataEntry = ChartDataEntry(x: Double(i),y: values[i])
-//				dataEntries.append(dataEntry)
-//			}
-//			
-//			let pieChartDataSet = PieChartDataSet(values: dataEntries, label: "Emotion")
-//			
-//			var dataSets = [IChartDataSet]()
-//			
-//			dataSets.append(pieChartDataSet)
-//			
-//			let pieChartData = PieChartData(dataSets: dataSets)
-//			pieChartView.data = pieChartData
-//			
-//			var colors: [UIColor] = []
-//			
-//			for i in 0..<dataPoints.count {
-//				let red = Double(arc4random_uniform(256))
-//				let green = Double(arc4random_uniform(256))
-//				let blue = Double(arc4random_uniform(256))
-//				
-//				let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
-//				colors.append(color)
-//			}
-//			
-//			pieChartDataSet.colors = colors
-//			
-//			
-//			let lineChartDataSet = LineChartDataSet(values: dataEntries, label: "Percentage")
-//			
-//			var datasets1 = [IChartDataSet]()
-//			
-//			datasets1.append(lineChartDataSet)
-//			
-//			let lineChartData = LineChartData(dataSets: datasets1)
-//			lineChartView.data = lineChartData
-//			
-//		}
-//	}
+		
+	}
+ 
 }
